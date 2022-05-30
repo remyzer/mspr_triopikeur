8edcbb1ef5cba157ab7deb813dd1c23beeea4426
Quentin Derriennic <quentin.derriennic@gmail.com>
2022-05-30 17:03:22 +0200

    avancé du style et structure

--- CanapiApp/src/main/webapp/App.vue
+++ CanapiApp/src/main/webapp/App.vue
@@ -16,35 +16,97 @@
 
 <body>
     <aside>
-        <div id="app" class="container-fluid">
+        <div class="container-fluid">
             <div class="row flex-nowrap">
-                <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 aside_content">
-                    <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                <div class="col-auto col-md-3 col-xl-3 px-sm-2 px-0 aside_content">
+                    <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100">
                         <a href="home.html"
-                            class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                            class="d-flex align-items-center pb-3 mb-md-0 me-md-auto">
                             <span class="fs-5 d-none d-sm-inline" style="text-align: center;">
                                 <img class="logo_canapi" alt="logo_canapi"
                                     src="./ressources/canapi_transparent.png">
                             </span>
                         </a>
-                        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                            <li>
-                                <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle" style="margin-top: 30px;">
-                                    <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Commandes à traiter</span> </a>
-                             
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0 align-middle" style="margin-top: 30px;">
-                                    <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Tableau</span></a>
-                            </li>
-                        </ul>
+                        <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2" style="margin: 0 auto; width: 100%;">
+                            <table class="table">
+                                <h2>Commandes à traiter</h2>
+                                <thead>
+                                  <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">DATE</th>
+                                    <th scope="col">CLIENT ID</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <th scope="row">1</th>
+                                    <td>13/08</td>
+                                    <td>00189</td>
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">2</th>
+                                    <td>17/07</td>
+                                    <td>00190</td>
+                                  </tr>
+                                  <tr style="border-bottom: hidden;">
+                                    <th scope="row">3</th>
+                                    <td>24/09</td>
+                                    <td>00191</td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                        </div>
+                        <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2" style="margin: 0 auto; width: 100%;">
+                            <table class="table table2">
+                                <h2>Stock Faible</h2>
+                                <thead>
+                                  <tr style="font-size: 0.7em;">
+                                    <th scope="col">PRODUIT ID</th>
+                                    <th scope="col">TYPE PRODUIT ID</th>
+                                    <th scope="col">QUANTITE</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <th scope="row">6</th>
+                                    <td>3</td>
+                                    <td>0001</td>
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">8</th>
+                                    <td>3</td>
+                                    <td>0002</td>
+                                  </tr>
+                                  <tr style="border-bottom: hidden;">
+                                    <th scope="row">2</th>
+                                    <td>3</td>
+                                    <td>0000</td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                        </div>
                         <span class="email_btn">
                             <button type="button" class="btn btn-outline-primary send_email_button">Envoyer un email</button>
                         </span>
                     </div>
                 </div>
                 <div class="content">
+                    <span><a class="logout" href="#">Deconnexion</a></span>
                     <h1 class="welcome_title">Bonjour User</h1>
+                    <div class="card dashboard">
+                        <div class="card-header dashboard_header">
+                          Tableau de bord
+                        </div>
+                        <div class="card-body">
+                            <div class="boutons_acces_principaux d-flex justify-content-around">
+                                <a href="#" class="btn btn-outline-secondary main_access_btn">Fichier Client</a>
+                                <a href="#" class="btn btn-outline-secondary main_access_btn">Listing Produits</a>
+                                <a href="#" class="btn btn-outline-secondary main_access_btn">Listing Commandes</a>
+                                <a href="#" class="btn btn-outline-secondary main_access_btn">Stats Commerciales</a>
+                            </div>
+                          <h5 class="card-title">Accéder rapidement :</h5>
+                        </div>
+                      </div>
                 </div>
             </div>
         </div>
@@ -52,6 +114,7 @@
     </aside>
 
 </body>
+
 
 </html>
 </template>
